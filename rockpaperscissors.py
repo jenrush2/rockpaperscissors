f0f5bc6b92d72eccefbf28d359382884af67c144
Jennifer Rush <jennifer.rush2@gmail.com>
2025-01-09 19:28:58 -0700

    fixes print statements

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -5,21 +5,21 @@ player_1 = player_1.lower()
 player_2 = player_2.lower()
 
 if player_1 == "rock" and player_2 == "rock":
-    print "Tie!"
+    print ("Tie!")
 elif player_1 == "rock" and player_2 == "paper":
-    print "Player 2 wins!"
+    print ("Player 2 wins!")
 elif player_1 == "rock" and player_2 == "scissors":
-    print "Player 1 wins!"
+    print ("Player 1 wins!")
 elif player_1 == "paper" and player_2 == "paper":
-    print "Tie!"
+    print ("Tie!")
 elif player_1 == "paper" and player_2 == "rock":
-    print "Player 1 wins!"
+    print ("Player 1 wins!")
 elif player_1 == "paper" and player_2 == "scissors":
-    print "Player 2 wins!"
+    print ("Player 2 wins!")
 elif player_1 == "scissors" and player_2 == "scissors":
-    print "Tie!"
+    print ("Tie!")
 elif player_1 == "scissors" and player_2 == "rock":
-    print "Player 2 wins!"
+    print ("Player 2 wins!")
 elif player_1 == "scissors" and player_2 == "paper":
-    print "Player 1 wins!"
+    print ("Player 1 wins!")
 
